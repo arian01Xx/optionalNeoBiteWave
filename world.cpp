e04c79bfca2889a6dc596babe94067f26bb240ea
Arian Joseph <arian_joseph@outlook.es>
2024-12-12 17:27:15 -0500

    first commit

---
+++ world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+	cout<<"i need help for god to my future"<<endl;
+	cout<<"please, i want to be famous, rich, smart, handsome"<<endl;
+	cout<<"why them and why me not"<<endl;
+	cout<<"trustly trustly i said you, everything you're dream, become reality"<<endl;
+	cout<<"amen"<<endl;
+	return 0;
+}
